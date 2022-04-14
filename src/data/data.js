21c116a42134e92bcdf6a1e9ac8d5724eb130ef9
ricky <ricky.chaggar@gmail.com>
2022-04-14 15:55:04 +0100

    Add site data
    Upload images and data.js content to be used to populate site

---
+++ src/data/data.js
@@ -0,0 +1,57 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  {
+    title: "Yosemite",
+    location: "USA",
+    googleMapsUrl: "https://goo.gl/maps/aK44KRqTiK9gkLpz8",
+    startDate: "25 May, 2014",
+    endDate: "11 June, 2014",
+    description:
+      "We spent three days in California's Yosemite national park as part of a road trip. It is still one the our favourite places to have ever visited with a particular highlight being the trek we did down from Glacier Point",
+    imageUrl: "Yosemite.jpg",
+  },
+
+  {
+    title: "Szechenyi Baths",
+    location: "Hungary",
+    googleMapsUrl: "https://goo.gl/maps/DxSBY9qVMXk81nqF9",
+    startDate: "11 November, 2019",
+    endDate: "19 November, 2019",
+    description:
+      "Located in Budapest Szechenyi offers outdoor thermal baths as well as steam rooms and ice cold plunge pools. We visited as part of a week in Budapest, our last pre-pandemic holiday.",
+    imageUrl: "Szechenyi-baths.jpg",
+  },
+
+  {
+    title: "Maddelena Island",
+    location: "Sardinia",
+    googleMapsUrl: "https://goo.gl/maps/Fu6hSBVowpX8tCc49",
+    startDate: "16 September, 2015",
+    endDate: "23 September, 2015",
+    description:
+      "Maddalena Island is located a short boat ride from Sardinia. It's beaches are pristine and the sea only increases very gently in depth making it perfect for paddling.",
+    imageUrl: "Maddalena.JPG",
+  },
+
+  {
+    title: "Fjords",
+    location: "Norway",
+    googleMapsUrl: "https://g.page/gudvangen_fjordtell?share",
+    startDate: "25 May, 2016",
+    endDate: "04 June, 2016",
+    description:
+      "After a rough crossing from Shetland my first sight of a Norweigen Fjord was on a warm, sunny May morning with crystal clear water reflecting a bright, blue sky. I absolutely intend to go back.",
+    imageUrl: "Gudvangen.jpg",
+  },
+
+  {
+    title: "Florence",
+    location: "Italy",
+    googleMapsUrl: "https://goo.gl/maps/i8EKVvXs1LKAt9759",
+    startDate: "06 May, 2011",
+    endDate: "14 May, 2011",
+    description:
+      "We spent a week just outside Florence on our honeymood. The Tuscan food, wine and gelatto were delicious and the history and culture of Florence live up to thier renown.",
+    imageUrl: "Florence.jpg",
+  },
+];
