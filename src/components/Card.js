05a9bc38b7e28bdd82fc8ec592344076d314d9a2
ricky <ricky.chaggar@gmail.com>
2022-04-25 09:30:23 +0100

    Change card text content to come from Props
    Card.js modified so that text content is displayed from props.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import mapPin from "../img/map-pin.png";
 import Yosemite from "../img/Yosemite.jpg";
 
 function Card(props) {
-  // console.log(props);
   return (
     <div className="card">
       <div className="card__content">
@@ -11,21 +10,18 @@ function Card(props) {
         <div className="card__text">
           <span className="location">
             <img src={mapPin} alt="Map Pin" className="card__location__pin" />
-            <span className="card__location__name">JAPAN</span>
+            <span className="card__location__name">{props.location}</span>
             <a
-              href="https://goo.gl/maps/aK44KRqTiK9gkLpz8"
+              href={props.googleMapsUrl}
               className="card__location__link"
             >
               View on Google Maps
             </a>
           </span>
-          <h1 className="card__name">Yosemite</h1>
-          <h4 className="card__dates">25 May, 2014 - 11 Junes, 2014</h4>
+          <h1 className="card__name">{props.title}</h1>
+          <h4 className="card__dates">{props.startDate} - {props.endDate}</h4>
           <p className="card__description">
-            We spent three days in California's Yosemite national park as part
-            of a road trip. It is still one the our favourite places to have
-            ever visited with a particular highlight being the trek we did down
-            from Glacier Point
+            {props.description}
           </p>
         </div>
       </div>
