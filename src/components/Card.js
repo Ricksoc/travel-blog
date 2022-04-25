1fdf2c9b922212e8ca7ca0bcfd436f88dec95e39
ricky <ricky.chaggar@gmail.com>
2022-04-25 10:07:24 +0100

    set img alt to use props.name

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,23 +5,24 @@ function Card(props) {
   return (
     <div className="card">
       <div className="card__content">
-        <img className="card__image" src={`../img/${props.imageUrl}`} alt="Yosemite Valley" />
+        <img
+          className="card__image"
+          src={`../img/${props.imageUrl}`}
+          alt={`${props.title}`}
+        />
         <div className="card__text">
           <span className="location">
             <img src={mapPin} alt="Map Pin" className="card__location__pin" />
             <span className="card__location__name">{props.location}</span>
-            <a
-              href={props.googleMapsUrl}
-              className="card__location__link"
-            >
+            <a href={props.googleMapsUrl} className="card__location__link">
               View on Google Maps
             </a>
           </span>
           <h1 className="card__name">{props.title}</h1>
-          <h4 className="card__dates">{props.startDate} - {props.endDate}</h4>
-          <p className="card__description">
-            {props.description}
-          </p>
+          <h4 className="card__dates">
+            {props.startDate} - {props.endDate}
+          </h4>
+          <p className="card__description">{props.description}</p>
         </div>
       </div>
       <hr className="card__break" />
